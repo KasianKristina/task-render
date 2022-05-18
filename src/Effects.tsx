8661238716ffe8a1e496f8d3a0ae21e8bb339165
Kristina <kasyankn@gmail.com>
2022-05-18 16:29:01 +0300

    Задание по рендерингу

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,24 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    let [name, setName] = useState(props.sourceId);
+    let [last_message, setLast_Message] = useState(-1);
+    let callback = (newMessage: number) => {
+        return setLast_Message(newMessage);
+    };
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        setName(props.sourceId);
+        setLast_Message(-1);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {name}: {last_message}
+        </div>
+    );
 }
